6f112c755d1b4d127240f0d9659abdbf2168fcff
meghna16694 <meghnagondaliya15@gmail.com>
2024-10-01 14:17:32 +0530

    first commit

---
+++ node1/index.js
@@ -0,0 +1,18 @@
+
+const http = require("http");
+const port = 9000;
+
+const portHandler = (req , res)=>{
+    res.write("<h1> sever started</h1>")
+
+    res.end()
+}
+
+const server = http.createServer(portHandler);
+
+server.listen(port,(err)=>{
+    err? console.log(err) : console.log("server started" + port)
+})
+
+
+
