b5116ed5623fe850adabd50d9bc219b55918a4ec
meghna16694 <meghnagondaliya15@gmail.com>
2024-11-18 14:03:07 +0530

    first commit

---
+++ SEODash/model/adminSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema({
+    activityName:{
+        type: String,
+        required: true 
+    },
+   date:{
+        type: String,
+        required: true 
+    },
+
+    time:{
+        type: String, 
+        required: true 
+    },  
+    activityType:{
+        type: String, 
+        required: true 
+    },
+    sport:{
+        type: String, 
+        required: true 
+    }, 
+    notes:{ 
+        type: String
+    } 
+
+
+
+})
+
+const admin = mongoose.model("SEODash",Schema)
+
+module.exports = admin
